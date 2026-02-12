320b6e61e68dd466bdb8937e34d306bb664eaa54
Sokolovsky Stepan <stepan.soko@list.ru>
2026-02-12 19:57:41 +0300

    add pythonpath

--- default.nix
+++ default.nix
@@ -10,6 +10,7 @@ pkgs.stdenv.mkDerivation {
   buildInputs = with pkgs; [
     python313
     python313Packages.numpy
+    python313Packages.scipy
     python313Packages.librosa
     python313Packages.matplotlib
     python313Packages.seaborn
@@ -21,9 +22,17 @@ pkgs.stdenv.mkDerivation {
     # Копируем исходники
     cp -r $src/src $out/lib/python3.13/site-packages/
 
-    # Создаем враппер
+    # Создаем враппер со всеми зависимостями в PYTHONPATH
     cat > $out/bin/freqs-vis <<EOF
     #!${pkgs.bash}/bin/bash
+    export PYTHONPATH="${with pkgs.python313Packages;
+      lib.makeSearchPath "lib/python3.13/site-packages" [
+        numpy
+        scipy
+        librosa
+        matplotlib
+        seaborn
+      ]}:\$PYTHONPATH"
     export PYTHONPATH=$out/lib/python3.13/site-packages:\$PYTHONPATH
     exec ${pkgs.python313}/bin/python -m src.main "\$@"
     EOF
