4dc06cdf689214e837e3b8dbe6879720df165436
Sokolovsky Stepan <stepan.soko@list.ru>
2026-02-12 19:49:32 +0300

    upd def

--- default.nix
+++ default.nix
@@ -1,10 +1,4 @@
-{
-  pkgs ? import <nixpkgs> {},
-  srcDir ? ./.,
-  subdir ? "",
-}: let
-  theSource = srcDir;
-
+{pkgs ? import <nixpkgs> {}}: let
   pythonPackage = pkgs.python313Packages.buildPythonPackage {
     pname = "freqs-vis";
     version = "1.0.0";
@@ -12,9 +6,9 @@
     src = ./.;
     format = "setuptools";
 
-    nativeBuildInputs = [
-      pkgs.python313Packages.setuptools
-      pkgs.python313Packages.wheel
+    nativeBuildInputs = with pkgs.python313Packages; [
+      setuptools
+      wheel
     ];
 
     propagatedBuildInputs = with pkgs.python313Packages; [
@@ -22,43 +16,17 @@
       matplotlib
       seaborn
     ];
-
-    meta = {
-      description = "Audio's frequences visualization tool";
-      license = pkgs.lib.licenses.mit;
-    };
-  };
-
-  pythonEnv = pkgs.python313.buildEnv.override {
-    extraLibs = [pythonPackage];
-    ignoreCollisions = true;
   };
 in
-  pkgs.stdenv.mkDerivation rec {
-    name = "freqs-vis";
-
-    dontUnpack = true;
-
-    buildInputs = [pythonEnv];
-
-    preInstall = ''
-      # Убеждаемся, что src скопирован
-      mkdir -p $out/lib/python3.13/site-packages
-      cp -r src $out/lib/python3.13/site-packages/
-    '';
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cat > $out/bin/${name} <<EOF
-      #!${pkgs.bash}/bin/bash
-      exec ${pythonEnv}/bin/python -m src.main "\$@"
-      EOF
-      chmod +x $out/bin/${name}
-    '';
-
-    meta = with pkgs.lib; {
-      description = "Audio frequencies visualization tool";
-      license = licenses.mit;
-      mainProgram = name;
-    };
-  }
+  pythonPackage.overrideAttrs (old: {
+    postInstall =
+      (old.postInstall or "")
+      + ''
+        mkdir -p $out/bin
+        cat > $out/bin/freqs-vis <<EOF
+        #!${pkgs.bash}/bin/bash
+        exec ${pythonPackage}/bin/python -m src.main "\$@"
+        EOF
+        chmod +x $out/bin/freqs-vis
+      '';
+  })
